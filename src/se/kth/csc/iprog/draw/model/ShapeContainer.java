717277e0c180b8a594f7c8c263352525d9e48946
cristi <cristi@kth.se>
2014-02-03 01:57:17 +0100

    returning the created shape

--- src/se/kth/csc/iprog/draw/model/ShapeContainer.java
+++ src/se/kth/csc/iprog/draw/model/ShapeContainer.java
@@ -6,53 +6,58 @@ import java.util.List;
 import java.util.Observable;
 
 public class ShapeContainer extends Observable {
-	
-	public static final int ELLIPSE = 10;
-	public static final int RECTANGLE = 20;
-	public static final int SEGMENT = 30;
-	
-	List<Shape> shapes = new ArrayList<Shape>(); 
-	
-	public void removeShape(Shape shape) throws IllegalArgumentException{
-		if(!shapes.contains(shape))
-			throw new IllegalArgumentException("Shape doesn't exist in the model");
-		shapes.remove(shape);
-		setChanged();
-		notifyObservers();
-	}
-	
-	public void addShape(int type, double x, double y, double h, double w) throws IllegalArgumentException{
-		Shape shape;
-		switch (type) {
-			case ELLIPSE:
-				shape = new Ellipse();
-				break;
-			case RECTANGLE:
-				shape = new Rectangle();
-			case SEGMENT:
-				shape = new Segment();
-			default:
-				throw new IllegalArgumentException("Unknown shape type"); 
-		}
-		shape.setX(x);
-		shape.setY(y);
-		shape.setH(h);
-		shape.setW(w);
-		shapes.add(shape);
-		setChanged();
-		notifyObservers();
-	}
-	
-	public List<Shape> getAllShapes(){
-		return Collections.unmodifiableList(shapes);
-	}
-	
-	public void modifyShape(Shape shape, double x, double y, double h, double w) throws IllegalArgumentException{
-		shape.setX(x);
-		shape.setY(y);
-		shape.setH(h);
-		shape.setW(w);
-		setChanged();
-		notifyObservers();
-	}
+
+    public static final int ELLIPSE = 10;
+
+    public static final int RECTANGLE = 20;
+
+    public static final int SEGMENT = 30;
+
+    List<Shape> shapes = new ArrayList<Shape>();
+
+    public void removeShape(Shape shape) throws IllegalArgumentException {
+        if (!shapes.contains(shape))
+            throw new IllegalArgumentException("Shape doesn't exist in the model");
+        shapes.remove(shape);
+        setChanged();
+        notifyObservers();
+    }
+
+    public Shape addShape(int type, double x, double y, double w, double h) throws IllegalArgumentException {
+        Shape shape;
+        switch (type) {
+            case ELLIPSE:
+                shape = new Ellipse();
+                break;
+            case RECTANGLE:
+                shape = new Rectangle();
+                break;
+            case SEGMENT:
+                shape = new Segment();
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown shape type");
+        }
+        shape.setX(x);
+        shape.setY(y);
+        shape.setH(h);
+        shape.setW(w);
+        shapes.add(shape);
+        setChanged();
+        notifyObservers();
+        return shape;
+    }
+
+    public List<Shape> getAllShapes() {
+        return Collections.unmodifiableList(shapes);
+    }
+
+    public void modifyShape(Shape shape, double x, double y, double w, double h) throws IllegalArgumentException {
+        shape.setX(x);
+        shape.setY(y);
+        shape.setH(h);
+        shape.setW(w);
+        setChanged();
+        notifyObservers();
+    }
 }
